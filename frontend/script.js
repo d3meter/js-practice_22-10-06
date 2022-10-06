d209b0be5a1c1653bc34de0c24b0029b52f6afcb
d3meter <mark.demeter3@gmail.com>
2022-10-06 12:01:02 +0200

    V3

--- frontend/script.js
+++ frontend/script.js
@@ -9,31 +9,3 @@ for (let i=0; i<span.length; i++) {
         span[i].classList.remove("newStyle");
     })
 }
-
-/* const span = document.querySelectorAll ("span");
-const container = document.querySelectorAll (".container");
-
-for (let i=0; i<span.length; i++) {
-    span[i].addEventListener("mouseover", function () {
-        span[i].style.color = `black`;
-        span[i].style.fontSize = `80px`;
-        span[i].style.transition = `all 2s ease`;
-    })
-    span[i].addEventListener("mouseout", function () {
-        span[i].style.color = ``;
-        span[i].style.fontSize = ``;
-    })
-} */
-
-/* container.addEventListener("mouseover", function () {
-    container.style.backgroundColor = `red`;
-    span.style.color = `black`;
-    span.style.fontSize = `80px`;
-    span.style.transition = `all 2s ease`;
-})
-
-container.addEventListener("mouseout", function () {
-    container.style.backgroundColor = ``;
-    span.style.color = ``;
-    span.style.fontSize = ``;
-}) */
